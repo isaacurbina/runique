838561844deb71bbdfe36b7a9ed3b34c7951150b
Isaac Urbina <7547113+isaacurbina@users.noreply.github.com>
2024-05-02 12:55:17 -0500

    Revert "revert last merge to main"
    This reverts commit 69778b3312de3fb83f1f564246ddbb41e3bdfd03.

--- build-logic/convention/build.gradle.kts
+++ build-logic/convention/build.gradle.kts
@@ -18,7 +18,6 @@ gradlePlugin {
             id = "runique.android.application"
             implementationClass = "AndroidApplicationConventionPlugin"
         }
-<<<<<<< HEAD
         register("androidApplicationCompose") {
             id = "runique.android.application.compose"
             implementationClass = "AndroidApplicationComposeConventionPlugin"
@@ -27,7 +26,5 @@ gradlePlugin {
             id = "runique.android.library"
             implementationClass = "AndroidLibraryConventionPlugin"
         }
-=======
->>>>>>> parent of 3e6bc40 (4.7.3 Adding AndroidApplicationComposeConventionPlugin (#6))
     }
 }
