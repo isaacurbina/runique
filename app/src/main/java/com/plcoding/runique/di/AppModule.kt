b6a995116ffa830faccc770fca5d8ca01ae369d8
Isaac Urbina <7547113+isaacurbina@users.noreply.github.com>
2024-05-11 12:53:15 -0500

    7.4 Implementing location tracking

--- app/src/main/java/com/plcoding/runique/di/AppModule.kt
+++ app/src/main/java/com/plcoding/runique/di/AppModule.kt
@@ -4,6 +4,8 @@ import android.content.SharedPreferences
 import androidx.security.crypto.EncryptedSharedPreferences
 import androidx.security.crypto.MasterKey
 import com.plcoding.runique.MainViewModel
+import com.plcoding.runique.RuniqueApp
+import kotlinx.coroutines.CoroutineScope
 import org.koin.android.ext.koin.androidApplication
 import org.koin.androidx.viewmodel.dsl.viewModelOf
 import org.koin.dsl.module
@@ -19,4 +21,7 @@ val appModule = module {
         )
     }
     viewModelOf(::MainViewModel)
+    single<CoroutineScope> {
+        (androidApplication() as RuniqueApp).applicationScope
+    }
 }
