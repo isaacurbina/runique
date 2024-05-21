ebd11b307a03a6637983e4ed99992684a271843b
Isaac Urbina <7547113+isaacurbina@users.noreply.github.com>
2024-05-20 20:05:04 -0500

    9.6_Saving-pending-syncs

---
+++ core/database/src/main/java/com/plcoding/core/database/dao/RunPendingSyncDao.kt
@@ -0,0 +1,36 @@
+package com.plcoding.core.database.dao
+
+import androidx.room.Dao
+import androidx.room.Query
+import androidx.room.Upsert
+import com.plcoding.core.database.entity.DeleteRunSyncEntity
+import com.plcoding.core.database.entity.RunPendingSyncEntity
+
+@Dao
+interface RunPendingSyncDao {
+
+    // region created runs
+    @Query("SELECT * FROM RunPendingSyncEntity WHERE userId=:userId")
+    suspend fun getAllRunPendingSyncEntities(userId: String): List<RunPendingSyncEntity>
+
+    @Query("SELECT * FROM RunPendingSyncEntity WHERE runId=:runId")
+    suspend fun getRunPendingSyncEntities(runId: String): RunPendingSyncEntity?
+
+    @Upsert
+    suspend fun upsertRunPendingSyncEntity(entity: RunPendingSyncEntity)
+
+    @Query("DELETE FROM RunPendingSyncEntity WHERE runId=:runId")
+    suspend fun deleteRunPendingSyncEntity(runId: String)
+    // endregion
+
+    // region deleted runs
+    @Query("SELECT * FROM  DeleteRunSyncEntity WHERE userId=:userId")
+    suspend fun getAllDeletedRunSyncEntities(userId: String): List<DeleteRunSyncEntity>
+
+    @Upsert
+    suspend fun upsertDeletedRunSyncEntity(entity: DeleteRunSyncEntity)
+
+    @Query("DELETE FROM DeleteRunSyncEntity WHERE runId=:runId")
+    suspend fun deleteDeleteRunSyncEntity(runId: String)
+    // endregion
+}
