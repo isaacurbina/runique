4d492acd5d2373d41cbb8b597bf0ea6e16e177ca
Isaac Urbina <7547113+isaacurbina@users.noreply.github.com>
2024-05-31 15:38:23 -0500

    3.8 Sending and receiving messages on the wearable (#65)

--- wear/run/presentation/src/main/java/com/plcoding/wear/run/presentation/TrackerEvent.kt
+++ wear/run/presentation/src/main/java/com/plcoding/wear/run/presentation/TrackerEvent.kt
@@ -1,5 +1,8 @@
 package com.plcoding.wear.run.presentation
 
+import com.plcoding.auth.presentation.UiText
+
 sealed interface TrackerEvent {
     data object RunFinished : TrackerEvent
+    data class Error(val message: UiText) : TrackerEvent
 }
